1ac5932055fd9618bf6967216b5a6a3309b1057c
Oluwatobii <bellotobi10@gmail.com>
2020-07-20 19:53:16 +0000

    Added 2 tables (entities) and data to the database

---
+++ migrations/assignments_submissions.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS assignment_submissions;
+DROP TABLE IF EXISTS assignments;
+
+CREATE TABLE assignments
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+);
+
+CREATE TABLE assignment_submissions
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  duration INTEGER,
+  submission_date DATE
+);
